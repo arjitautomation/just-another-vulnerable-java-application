3bc6568722200e428dfe357729a137f530366eea
Arjit Srivastava <41647886+arjitautomation@users.noreply.github.com>
2023-04-05 14:12:49 +0530

    Update create_task.ps1

--- create_task.ps1
+++ create_task.ps1
@@ -1,4 +1,5 @@
 
+$pactoken=$args[0]
 $organizationName = "arjitdevops"
 $projectName = "Devops"
 $adoHeader = @{Authorization=("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f "",$pacToken))))}
